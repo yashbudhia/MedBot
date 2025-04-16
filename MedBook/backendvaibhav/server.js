ad0ebf358cdebe34ceeffaf21ab38309a4d126f9
yashbudhia <yeshbudhia@gmail.com>
2025-04-16 21:45:45 +0530

    env-change

--- MedBook/backendvaibhav/server.js
+++ MedBook/backendvaibhav/server.js
@@ -10,8 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-const dburl =
-  "mongodb://localhost:27017/medbook"; // your Mongo URL
+const dburl = "mongodb+srv://yashbudhia:khuljas1ms1m@cluster0.nnafmtq.mongodb.net/medbook-users";
 
 mongoose
   .connect(dburl, {})
